33ef2a54bb9538daa7dd664e89b77ecf8484e1f7
jonsnowsalju <94001677+jonsnowsalju@users.noreply.github.com>
2021-11-15 21:36:27 +0700

    Update ghost.js

--- ghost.js
+++ ghost.js
@@ -8,17 +8,19 @@ import cron from "cron";
 
 module.exports = ghost = (client) =>{
 	let invite = new cron.CronJob(
-		"*/1 * * * *",
-			async () => {
-				let randomTimer = Math.floor(Math.random() * 5000) +  1000;
-				setTimeout(() => {
-					client.send('!rank', config.botChannel).then((msg) => setTimeout(() => {
-						client.delete_message(msg.id, config.botChannel);}, 100)
-					};
-				}, randomTimer);
-			},
-			null,
-			true,
-			config.timezone
+	"*/1 * * * *",
+	async () => {
+		let randomTimer = Math.floor(Math.random() * 5000) +  1000;
+		setTimeout(() => {
+		client.send('!rank', config.botChannel).then((msg) =>
+			setTimeout(() => {
+			client.delete_message(msg.id, config.botChannel);
+			}, 100)
+		};
+		},randomTimer)
+	},
+	null,
+	true,
+	config.timezone
 	};
 };
