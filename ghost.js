bb9121589cfa7905a67b9c72d83ad78343fea1a7
jonsnowsalju <94001677+jonsnowsalju@users.noreply.github.com>
2021-11-15 21:28:58 +0700

    Update ghost.js

--- ghost.js
+++ ghost.js
@@ -7,19 +7,18 @@ const client = new Discord.Client(config.token);
 import cron from "cron";
 
 module.exports = ghost = (client) =>{
-		let invite = new cron.CronJob(
-			"*/1 * * * *",
+	let invite = new cron.CronJob(
+		"*/1 * * * *",
 			async () => {
 				let randomTimer = Math.floor(Math.random() * 5000) +  1000;
 				setTimeout(() => {
 					client.send('!rank', config.botChannel).then((msg) => setTimeout(() => {
-						client.delete_message(msg.id, config.botChannel);
-					},100)
-				};
-			}, randomTimer);
-		},
-		null,
-		true,
-		config.timezone
+						client.delete_message(msg.id, config.botChannel);}, 100)
+					};
+				}, randomTimer);
+			},
+			null,
+			true,
+			config.timezone
 	};
 };
